922b442fab2e88898f859a53ffcd16d46cdcc088
Bryan <brymac@gmail.com>
2015-06-22 23:18:53 -0400

    Stretch 01 complete

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -1,41 +1,29 @@
 class Changer
   def initialize
-
+    @change = []
+    @total_remaining
   end
-
-
-#Quarters, dimes, nickels, pennies
 
   def self.make_change(total)
-    change = []
-    total_remaining = total
-
-    num_quarters = (total_remaining / 25).to_i
-    num_quarters.times do
-      change << 25
-    end
-    total_remaining -= (num_quarters * 25)
-
-    num_dimes = (total_remaining / 10).to_i
-    num_dimes.times do
-      change << 10
-    end
-    total_remaining -= (num_dimes * 10)
-
-    num_nickels = (total_remaining / 5).to_i
-    num_nickels.times do
-      change << 5
-    end
-    total_remaining -= (num_nickels * 5)
-
-    num_pennies = (total_remaining / 1).to_i
-    num_pennies.times do
-      change << 1
-    end
-    total_remaining -= (num_pennies * 1)
-
-
-    change
+    @total_remaining = total
+    @change = []
+    @total_remaining = self.subtract_change(@total_remaining, 25)
+    @total_remaining = self.subtract_change(@total_remaining, 10)
+    @total_remaining = self.subtract_change(@total_remaining, 5)
+    @total_remaining = self.subtract_change(@total_remaining, 1)
+    @change
   end
+
+
+  def self.subtract_change(total_remaining, coin_amount)
+    total_remaining = total_remaining.to_i
+    coin_amount = coin_amount.to_i
+    num_coins = (total_remaining / coin_amount)
+    num_coins.times do
+      @change << coin_amount
+    end
+    total_remaining -= (num_coins * coin_amount)
+  end
+
 
 end
